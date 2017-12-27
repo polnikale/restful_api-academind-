067ddd6c6d8d0df31eef2b1b5ccd92f93785aa17
polnikale <nikpolale@gmail.com>
2017-12-27 19:32:31 +0200

    i've just started making a RESTful API with Node.JS by Academind

---
+++ api/routes/products.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling get request to /products'
+    })
+});0
+
+router.post('/', (req, res, next) => {
+    const product = {
+        name: req.body.name,
+        price: req.body.price
+    }
+    res.status(201).json({
+        message: 'Handling post request to /products',
+        createdProduct: product
+    })
+});
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    if (id === 'special') {
+        res.status(200).json({
+            message: 'You discovered the special ID',
+            id
+        });
+    } else {
+        res.status(200).json({
+            message: 'You passed an ID'
+        })
+    }
+});
+
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Updated product!'
+    });
+});
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Deleted product!'
+    });
+});
+
+module.exports = router;
